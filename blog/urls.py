2fdecc5f61cfa07cf2984dfa310ced6dc37dcc16
JayCho <sc8556@naver.com>
2025-06-12 23:17:25 +0900

    Add function tag and post form

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import  views
 from .views import PostDetail
 
 urlpatterns = [
+    path('update_post/<int:pk>/',views.PostUpdate.as_view()),
+    path('create_post/',views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', PostDetail.as_view()),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 import posts
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.context_processors import request
-from django.views.generic import ListView, DetailView
+from django.utils.text import slugify
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
 
 
 class PostList(ListView):
@@ -60,3 +63,71 @@ def tag_page(request, slug):
 
         }
     )
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title','hook','content','head_image','file_upload','category']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
+            form.instance.author = current_user
+            response = super(PostCreate, self).form_valid(form)
+
+            # ✅ 수정된 부분
+            tag_str = self.request.POST.get('tag_str', '').strip()
+            if tag_str:
+                tag_str = tag_str.replace(',', ';')
+                tags_list = [t.strip() for t in tag_str.split(';') if t.strip()]
+                for t in tags_list:
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
+        else:
+            return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title','hook','content','head_image','file_upload','category']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list).strip()
+        return context
+
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
+
+        tag_str = self.request.POST.get('tag_str', '').strip()  # ✅ 수정됨
+        if tag_str:
+            tag_str = tag_str.replace(',', ';')
+            tags_list = [t.strip() for t in tag_str.split(';') if t.strip()]
+
+            for t in tags_list:
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
